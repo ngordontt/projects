7f79595dce134d11467745daed44ba8551b88af3
Neil P. Gordon <neilgordon@hotmail.com>
2018-02-01 09:13:21 -0500

    2/1

---
+++ Java/totalcost/src/totalCost.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class totalCost {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double meal_cost = in.nextDouble();
+        int tip_percent = in.nextInt();
+        int tax_percent = in.nextInt();
+        in.close();
+    }
+}
