22c02725b098bdadb5dfc4698b77d79513537969
neil <neilgordon@hotmail.com>
2018-02-08 21:28:01 -0500

    2/8

--- Python/webscrape/HCSO_for_email/HCSO.py
+++ Python/webscrape/HCSO_for_email/HCSO.py
@@ -3,9 +3,9 @@ import urllib.parse
 from bs4 import BeautifulSoup
 import re
 
-
 url = 'http://apps.hcso.org/PropertySale.aspx'
 
+#needed header to soppf browser
 hdr = {
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding':'gzip, deflate',
@@ -16,21 +16,23 @@ hdr = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
 }
 
-req = urllib.request.Request(url,headers=hdr)
+#1st page pull,
+req = urllib.request.Request(url,headers=hdr) # includes urls and headers
 response =urllib.request.urlopen(req)
 f = response.read()
 
+#parse page
 soup_dummy = BeautifulSoup(f, 'html.parser')
 
 
-# parse and retrieve two vital form values
+# parse and retrieve needed form values
 viewstate = soup_dummy.select("#__VIEWSTATE")[0]['value']
 viewstategen = soup_dummy.select("#__VIEWSTATEGENERATOR")[0]['value']
 viewValid = soup_dummy.select("#__EVENTVALIDATION")[0]['value']
 viewTSM= soup_dummy.select("#ToolkitScriptManager1_TSM")[0]['value']
 
 
-# search for the string 'input' to find the form data
+# form data to simulate clicking on upcomming forclosure button
 formData = (
     ('__VIEWSTATE', viewstate),
     ('__VIEWSTATEGENERATOR', viewstategen),
@@ -39,31 +41,33 @@ formData = (
     ('__EVENTVALIDATION', viewValid)
 )
 
-# 2nd HTTP request with form data
+# 2nd HTTP request with form data to simulate clicking on upcomming forclosure button
 data = urllib.parse.urlencode(formData)
 data = data.encode('ascii')  # data should be bytes
 req = urllib.request.Request(url, data, headers=hdr)
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
 
-# parse page to get form data
+# parse page to get form data 
 soup = BeautifulSoup(the_page, 'html.parser')
+
+#re-retrive form fields incase they changed
 viewstate = soup.select("#__VIEWSTATE")[0]['value']
 viewstategen = soup.select("#__VIEWSTATEGENERATOR")[0]['value']
 viewValid = soup.select("#__EVENTVALIDATION")[0]['value']
 
 # find value for 3 date choice and update form data
-Foreclosure_dates = soup.find_all('ul')
-Foreclosure_date = Foreclosure_dates[8].find_all('li')[3].text.strip()
-Foreclosure_ch = re.findall(r'[^/]+(?://[^/]*)*', Foreclosure_date)
-date = Foreclosure_ch[0]+"%2F"+Foreclosure_ch[1]+"%2F"+Foreclosure_ch[2]
-eventag = {"type":1,"index":3,"text":"","value":""}
-ddlDate = {"enabled":"","logEntries":[],"selectedIndex":3,"selectedText":"","selectedValue":""}
+Foreclosure_dates = soup.find_all('ul') #use soup to find tags that match date containing tags
+Foreclosure_date = Foreclosure_dates[8].find_all('li')[3].text.strip() #item 8 with tage li and item 3 to text and striped
+Foreclosure_ch = re.findall(r'[^/]+(?://[^/]*)*', Foreclosure_date) #use regex to pick only what is between ie 2/34/90
+date = Foreclosure_ch[0]+"%2F"+Foreclosure_ch[1]+"%2F"+Foreclosure_ch[2] #join date in format need for forms
+eventag = {"type":1,"index":3,"text":"","value":""} #insert date into form field value
+ddlDate = {"enabled":"","logEntries":[],"selectedIndex":3,"selectedText":"","selectedValue":""}#insert date into form field value
 eventag['text'] = date
 ddlDate['selectedText'] = date
 ddlDate['enabled'] = 'true'
 
-# formdata with date choice
+# form data with fields need to simulate picking date in upcomming forclosure drop down
 formData = (
     ('__VIEWSTATE', viewstate),
     ('__VIEWSTATEGENERATOR', viewstategen),
@@ -74,7 +78,7 @@ formData = (
     ('ddlDate_ClientState',ddlDate)
 )
 
-# 3rd HTTP request with form data
+# 3rd HTTP request with form data to simulate picking date in upcomming forclosure drop down
 data = urllib.parse.urlencode(formData)
 data = data.encode('ascii')  # data should be bytes
 req = urllib.request.Request(url, data, headers=hdr)
@@ -83,11 +87,13 @@ with urllib.request.urlopen(req) as response:
 
 # parse page to get form data
 soup = BeautifulSoup(the_page, 'html.parser')
+
+#re-retrive form fields incase they changed
 viewstate = soup.select("#__VIEWSTATE")[0]['value']
 viewstategen = soup.select("#__VIEWSTATEGENERATOR")[0]['value']
 viewValid = soup.select("#__EVENTVALIDATION")[0]['value']
 
-# formdata with go button click
+# formdata need to simulate go button click
 formData = (
     ('__VIEWSTATE', viewstate),
     ('__VIEWSTATEGENERATOR', viewstategen),
@@ -98,7 +104,7 @@ formData = (
     ('ddlDate_ClientState','')
 )
 
-# 4th HTTP request with form data
+# 4th HTTP request with form data to simulate go button click
 data_fin = urllib.parse.urlencode(formData)
 data_fin = data_fin.encode('ascii')  # data should be bytes
 req_fin = urllib.request.Request(url, data_fin, headers=hdr)
@@ -109,10 +115,26 @@ with urllib.request.urlopen(req_fin) as response:
 # parse page to get form data
 soup_fin = BeautifulSoup(the_page_fin, 'html.parser')
 
+#find table rows with date
 listings = soup_fin.findAll("tr")
 
+#use first recored as headers for CSV field
 heading = re.findall(r'col">(.*?)<\/th>',str(listings[0]))
 # ['Case NO', 'Plaintiff', 'Defendant', 'Address', 'Atty Name', 'Atty Phone', 'Appraisal ', 'MIn Bid', 'Township', 'WD', 'Sale Date']
+
+headers_csv = list()
+for head in heading:
+    headers_csv.append(head)
+
+#regex expression for picking values
+regex = r'<td>(.*?)<\/td>'
+
+for x in range(1, len(listings)): #loop for length of listings, starting from 1 until len
+    row = list() #create empty list
+    heading_x = re.findall(regex,str(listings[x]))
+    for i in [a for a in range(len(heading_x)) if a != 9]: #loop skipping item 9
+        row.append(heading_x[i]) #add to list
+    print(row)
 
 #file process
 filename = "HCSO-complete.csv"
@@ -121,7 +143,5 @@ headers_csv = heading[1],
 
 f.write(headers)
 
-headers_csv = [""]
-for head in heading:
-    headers_csv.append(head)
+
 
